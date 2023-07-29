6bdff16dc495f0c6124f046364ab91c9aa1706a1
Luchy-B <okparabon@gmail.com>
2023-07-29 21:50:56 +0100

    Fix error

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,13 @@ module HelloRailsBackEnd
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'localhost:3000' # Adjust this to match your React app's URL
+        resource '*',
+          headers: :any,
+          methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      end
+    end
   end
 end
